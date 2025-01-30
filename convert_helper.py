20ac784e003e93001e54712c31e461c2e2788d63
JF-Oberfläche <kite.loop@web.de>
2025-01-30 13:23:26 +0100

    changes 1

--- convert_helper.py
+++ convert_helper.py
@@ -1,6 +1,9 @@
 import subprocess
-import tkinter
 import customtkinter
+import markdown
+from tkinterweb import HtmlFrame
+import os
+
 
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("green")
@@ -9,7 +12,10 @@ app = customtkinter.CTk()
 app.geometry("720x480")
 app.title("WikiConvert")
 
-linkArray = []
+folder_path1 = "./landing"
+folder_path = os.path.abspath(folder_path1)
+linkList = []
+markdown_files = []
 
 
 #UI Methods
@@ -27,21 +33,58 @@ def convert(linklist):
             ]
         )
 
+def display_markdown(filepath, tab_name):
+    try:
+        with open(filepath, "r", encoding="utf-8") as f:
+            markdown_content = f.read()
+            html_content = markdown.markdown(markdown_content)
+
+            if tab_name not in app.tabview.tab_names():
+                app.tabview.add(tab_name)  # Add the tab if it doesn't exist
+                frame = HtmlFrame(app.tabview.tab(tab_name))
+                frame.pack(fill="both", expand=True)
+            else:
+                frame = app.tabview.tab(tab_name).winfo_children()[0] #get the HtmlFrame
+
+            frame.set_content(html_content)
+
+    except FileNotFoundError:
+        print(f"Markdown file not found: {filepath}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
 def get_link():
     text_link = link.get()
-    linkArray.append(text_link)
+    linkList.append(text_link)
     print(text_link)
-    print(linkArray)
+    print(linkList)
 
 def convert_link():
     label.pack_forget()
-    if len(linkArray) > 0:
+    if len(linkList) > 0:
         try:
-            convert(linkArray)
+            convert(linkList)
+
+            app.tabview = customtkinter.CTkTabview(app)
+            app.tabview.pack(fill="both", expand=True)
+
+            if os.path.isdir(folder_path):
+                for filename in os.listdir(folder_path):
+                    file_path = os.path.join(folder_path, filename)  # Get the full file path
+                    if os.path.isfile(file_path):
+                        markdown_files.append(file_path)
+            else:
+                print(f"The folder '{folder_path}' does not exist.")
+
+            for tab_name, filepath in markdown_files.items():
+                display_markdown(filepath, tab_name)
+
         except Exception as e:
             print(f"An error occurred: {e}")
         finally:
-            del linkArray[:]
+            del linkList[:]
             print("List cleared")
     else:
         label.pack()
@@ -49,7 +92,6 @@ def convert_link():
 
 #UI Elements
 link = customtkinter.CTkEntry(app, placeholder_text="Insert a Wikipedia Link here", width=200, height=40)
-
 
 button = customtkinter.CTkButton(app, text="Add Link to List", command=get_link)
 
